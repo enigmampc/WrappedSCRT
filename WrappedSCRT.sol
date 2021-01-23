15682ad183bd4c42f236fb9c571d55d57dc19557
TomL94 <langer.tom7@gmail.com>
2021-01-23 20:39:36 +0200

    Remove fixed supply and mint/burn by demand

--- WrappedSCRT.sol
+++ WrappedSCRT.sol
@@ -8,22 +8,22 @@ import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/token/ERC20/ERC20Burnable.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract WrappedSCRT is Context, AccessControl, ERC20Pausable {
+contract WrappedSCRT is Context, AccessControl, ERC20Burnable, ERC20Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     /**
      * @dev Grants `DEFAULT_ADMIN_ROLE` and `PAUSER_ROLE` to the
-     * account that deploys the contract.
+     * account that deploys the contract. Grnats `MINTER_ROLE` to the bridge.
      *
      * See {ERC20-constructor}.
      */
-    constructor(uint256 totalSupply, address bridge) public ERC20("Wrapped SCRT", "WSCRT") {
-        _mint(bridge, totalSupply);
-        
+    constructor(address bridge) public ERC20("Wrapped SCRT", "WSCRT") {
         _setupDecimals(6);
         
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, bridge);
     }
 
     /**
@@ -52,5 +52,20 @@ contract WrappedSCRT is Context, AccessControl, ERC20Pausable {
     function unpause() public virtual {
         require(hasRole(PAUSER_ROLE, _msgSender()), "WrappedSCRT: must have pauser role to unpause");
         _unpause();
+    }
+    
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
+        if (hasRole(MINTER_ROLE, _msgSender())) {
+            _mint(_msgSender(), amount);
+        } else if (hasRole(MINTER_ROLE, recipient)) {
+            _burn(sender, amount);
+            return;
+        } 
+        
+        super._transfer(sender, recipient, amount);
+    }
+    
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
