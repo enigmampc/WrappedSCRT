a87531f886bd8d921210cfa4d9d8119f15c5a70e
TomL94 <langer.tom7@gmail.com>
2021-01-24 16:22:40 +0200

    Etherscan verification

--- contracts/WrappedSCRT.sol
+++ contracts/WrappedSCRT.sol
@@ -2,11 +2,11 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/access/AccessControl.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/GSN/Context.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/token/ERC20/ERC20.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/token/ERC20/ERC20Burnable.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v3.3.0/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract WrappedSCRT is Context, AccessControl, ERC20Burnable, ERC20Pausable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -20,7 +20,7 @@ contract WrappedSCRT is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      */
     constructor(address bridge) public ERC20("Wrapped SCRT", "WSCRT") {
         _setupDecimals(6);
-        
+
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
         _setupRole(MINTER_ROLE, bridge);
@@ -36,7 +36,10 @@ contract WrappedSCRT is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      * - the caller must have the `PAUSER_ROLE`.
      */
     function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "WrappedSCRT: must have pauser role to pause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "WrappedSCRT: must have pauser role to pause"
+        );
         _pause();
     }
 
@@ -50,11 +53,18 @@ contract WrappedSCRT is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      * - the caller must have the `PAUSER_ROLE`.
      */
     function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "WrappedSCRT: must have pauser role to unpause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "WrappedSCRT: must have pauser role to unpause"
+        );
         _unpause();
     }
-    
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual override {
         if (hasRole(MINTER_ROLE, sender)) {
             _mint(recipient, amount);
         } else if (hasRole(MINTER_ROLE, recipient)) {
@@ -63,8 +73,12 @@ contract WrappedSCRT is Context, AccessControl, ERC20Burnable, ERC20Pausable {
             super._transfer(sender, recipient, amount);
         }
     }
-    
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20, ERC20Pausable) {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
